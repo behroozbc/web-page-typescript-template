dcd767e18b8bd18036cf4f55d62a35736c970da5
behrooz bozorg chami <behroozbozorgchami@hotmail.com>
2021-06-10 19:10:01 +0430

    remove lodash dependencies

--- ts/src/main.ts
+++ ts/src/main.ts
@@ -1,13 +1,7 @@
-import template from 'lodash/template';
 
 const outputElement = document.getElementById('output');
 if (outputElement) {
-  var compiled = template(`
-    <h1><%- heading %></h1>
-    Current date and time: <%- dateTimeString %>
-  `.trim());
-  outputElement.innerHTML = compiled({
-    heading: 'ts-demo-webpack',
-    dateTimeString: new Date().toISOString(),
-  });
+  outputElement.innerHTML = `<h1>ts-demo-webpack</h1>
+  Current date and time: ${new Date().toISOString()}
+  `;
 }
